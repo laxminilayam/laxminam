a6aa1281585ce2e4c1fd13f305069a1c1d61fb95
laxminilayam <bokkasrinivas.parkville@gmail.com>
2025-02-15 07:32:04 +0000

    number change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -54,7 +54,7 @@ const Contact = () => {
                     >
                         <h3 className="text-3xl font-bold text-blue-700 mb-4">Call Now:</h3>
                         <p className="text-gray-600 font-merriweather">
-                            Phone: +91 70751 55555<br />
+                            Phone: +91 94409 96805<br />
 
                         </p>
 
